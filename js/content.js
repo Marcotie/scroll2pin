0acc156828625159bfb5ba870f24cc74bd02eab0
emilytimer <by0807103@163.com>
2019-08-22 22:35:51 +0800

    init

---
+++ js/content.js
@@ -0,0 +1,12 @@
+var div = document.createElement( 'div' );
+div.id = 'my-pin-box';
+
+document.body.appendChild( div );
+
+
+
+div.innerHTML=`
+<div class='iconfont icon-up'>
+</div>
+<div class='iconfont icon-down'>
+</div>`
